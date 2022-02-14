0345c5138a3a4bc5aeb78723a07690039157354c
Karel <snakehead007@pm.me>
2022-02-14 11:41:43 +0100

    intial commit - Next.js + tailwind + Typescript :rocket:

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Link from 'next/link'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+    <Head>
+      <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="crossorigin"></link>
+      <link href="https://fonts.googleapis.com/css2?family=Rajdhani:wght@300&display=swap" rel="stylesheet"></link>
+    </Head>
+    <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
