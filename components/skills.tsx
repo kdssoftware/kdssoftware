af7fe444db574d99388a5c51b637545e447afcd5
Karel <snakehead007@pm.me>
2022-02-18 15:56:59 +0100

    fixed missing key props:

--- components/skills.tsx
+++ components/skills.tsx
@@ -11,13 +11,13 @@ const Skills: NextPage = () => {
             <section className="w-full flex">
                 {
                     skilldata.map((skill, index) => (
-                        <div className="w-1/4 flex flex-col content-start justify-start" >
+                        <div key={index} className="w-1/4 flex flex-col content-start justify-start" >
                             <h2 className="text-2xl text-center font-bold text-sky-500 ">{skillTypes[index]}:</h2>
                             <div className="border-2 rounded-lg border-sky-800 w-px[90px] m-3"></div>
                             <div className="flex flex-col content-start px-8">
                                 {
-                                    skill.map((item) => (
-                                        <div className="flex flex-row pt-1 ">
+                                    skill.map((item,i) => (
+                                        <div key={index+"-"+i} className="flex flex-row pt-1 ">
                                             <span className="block w-2/5 font-bold text-lg">{item.name}</span>
                                             <div className="w-3/5 bg-gray-500 rounded-full h-2.5 mt-2 ">
                                                 <div className="bg-lime-500 h-2.5 rounded-full duration-400 transition-all ease-in" style={{width: item.percentage+"%"}} ></div>
